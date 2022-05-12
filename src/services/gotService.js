ca6106143fbdc985715158d169eacfacbe57539f
Anton Demin <anton.demin@aalto.fi>
2022-05-12 03:33:07 +0300

    uploading

---
+++ src/services/gotService.js
@@ -0,0 +1,71 @@
+export default class GotService {
+
+    constructor(){
+        this._apiBase = 'https://www.anapioficeandfire.com/api';
+    }
+    async getResourse(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status% ${res.status}`);
+        }
+    
+        return await res.json();
+    };
+
+    async getAllCharacters() {
+        const res = await this.getResourse('/characters?page=5&pageSize=10');
+        return res.map(this._transformCharacter)
+    }
+
+    async getCharacter(id) {
+        const character = await this.getResourse(`/characters/${id}`);
+        return this._transformCharacter(character)
+    }
+
+    getAllHouses() {
+        return this.getResourse(`/houses/`);
+    }
+
+    getHouse(id) {
+        return this.getResourse(`/houses/${id}`);
+    }
+
+    getAllBooks() {
+        return this.getResourse(`/books/`);
+    }
+
+    getbook(id) {
+        return this.getResourse(`/books/${id}`);
+    }
+
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
+    }
+
+    _transformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publisher: book.publisher,
+            released: book.released
+        }
+    }
+}
